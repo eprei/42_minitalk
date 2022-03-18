e3604bf3b182e761b669e11096eab2f3852728c4
Emiliano Presa custodio <epresa-c@c2r9s6.42lausanne.ch>
2022-03-18 11:10:48 +0100

    norminette1

--- client.c
+++ client.c
@@ -1,66 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epresa-c <epresa-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 10:54:36 by epresa-c          #+#    #+#             */
+/*   Updated: 2022/03/18 11:05:54 by epresa-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
-int ft_atoi_simplified(const char *str)
+int	ft_atoi_simplified(const char *str)
 {
-    int result;
+	int	result;
 
-    result = 0;
-    while (*str)
-        result = result * 10 + (*str++ - '0');
-    return (result);
+	result = 0;
+	while (*str)
+		result = result * 10 + (*str++ - '0');
+	return (result);
 }
 
-char    *ft_convert_char_to_bits(char c)
+char	*ft_convert_char_to_bits(char c)
 {
-    static char bits[8] = {0,0,0,0,0,0,0,0};
-    int i;
+	static char	bits[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+	int			i;
 
-    i = 7;
-    while (i + 1 > 0)
-    {
-        if (c % 2 == 1)
-            bits[i--] = '1';
-        else
-            bits[i--] = '0';
-        c = c / 2;
-    }
-    return (bits);
+	i = 7;
+	while (i + 1 > 0)
+	{
+		if (c % 2 == 1)
+			bits[i--] = '1';
+		else
+			bits[i--] = '0';
+		c = c / 2;
+	}
+	return (bits);
 }
 
-void ft_send_bit(char *bits, int pid)
+void	ft_send_bit(char *bits, int pid)
 {
-    size_t i;
-    
-    i = 0;
-    while(bits[i] != '\0')
-    {
-        if (bits[i] == '1')
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-        i++;
-		usleep(50);
-    }
-}
-
-int main(int argc, char **argv)
-{
-    int pid;
-    char *bits;
-    int i;
+	size_t	i;
 
 	i = 0;
-    if (argc != 3)
-        write(1, "Correct use of the client program: ./client [server pid] [string to send]\n" , 74);
-    else
-    {
-    	pid = ft_atoi_simplified(argv[1]);
-        while (argv[2][i] != '\0')
-        {
-            bits = ft_convert_char_to_bits(argv[2][i++]);
-            ft_send_bit(bits, pid);
+	while (bits[i] != '\0')
+	{
+		if (bits[i] == '1')
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i++;
+		usleep(50);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		pid;
+	char	*bits;
+	int		i;
+
+	i = 0;
+	if (argc != 3)
+		write(1, "Correct use of the client program: \
+		./client [server pid] [string to send]\n", 74);
+	else
+	{
+		pid = ft_atoi_simplified(argv[1]);
+		while (argv[2][i] != '\0')
+		{
+			bits = ft_convert_char_to_bits(argv[2][i++]);
+			ft_send_bit(bits, pid);
 			usleep(80);
-        }
-    }
+		}
+	}
 	return (0);
 }
--- server.c
+++ server.c
@@ -1,20 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epresa-c <epresa-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 10:55:21 by epresa-c          #+#    #+#             */
+/*   Updated: 2022/03/18 11:05:47 by epresa-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
-void ft_print_bits(char *bits)
+void	ft_print_bits(char *bits)
 {
-	int pow;
-	char c;
-	int i;
+	int		pow;
+	char	c;
+	int		i;
 
 	pow = 1;
 	c = 0;
 	i = 7;
 	while (i + 1 > 0)
-    {
-        c = c + (pow * (bits[i] - '0'));
-        pow = pow * 2;
-        i--;
-    }
+	{
+		c = c + (pow * (bits[i] - '0'));
+		pow = pow * 2;
+		i--;
+	}
 	write(1, &c, 1);
 }
 
@@ -23,43 +35,43 @@ int	ft_putchar(char c)
 	return (write(1, &c, 1));
 }
 
-void	ft_putnbr(unsigned int i, int base)
+void	ft_putnbr(unsigned int i)
 {
 	char	*base_set;
 
 	base_set = "0123456789";
-	if (i / base > 0)
-		ft_putnbr(i / base, base);
-	ft_putchar(base_set[i % base]);
+	if (i / 10 > 0)
+		ft_putnbr(i / 10);
+	ft_putchar(base_set[i % 10]);
 }
 
-void ft_read_binary(int sig)
+void	ft_read_binary(int sig)
 {
-    static char bits[8] = {0,0,0,0,0,0,0,0};
-    static int i = 0;
+	static char	bits[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+	static int	i = 0;
 
-    if (sig == 30)
-        bits[i++] = '1';
-    else
-        bits[i++] = '0';
-    if (i == 8)
+	if (sig == 30)
+		bits[i++] = '1';
+	else
+		bits[i++] = '0';
+	if (i == 8)
 	{
-        ft_print_bits(bits);
-        i = 0;
-        while (i < 8)
-          bits[i++] = 0;
-        i = 0;
-    }
+		ft_print_bits(bits);
+		i = 0;
+		while (i < 8)
+			bits[i++] = 0;
+		i = 0;
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    write(1, "Server PID: ", 13);
-    ft_putnbr(getpid(), 10);
-    write(1, "\n", 1);
+	write(1, "Server PID: ", 13);
+	ft_putnbr(getpid());
+	write(1, "\n", 1);
 	signal(SIGUSR1, ft_read_binary);
 	signal(SIGUSR2, ft_read_binary);
 	while (1)
 		pause();
-    return (0);
+	return (0);
 }
